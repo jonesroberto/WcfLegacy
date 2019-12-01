b92c66ebf7893aab7972489dcc13cd4af47b3cf3
jones <jones@DESKTOP-LDHUHUF>
2019-12-01 02:55:09 -0300

    Add project files.

---
+++ WcfLegacy/MyLegacyService.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfLegacy
+{
+    public class MyLegacyService : IMyLegacyService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public decimal Calculate(decimal a, decimal b)
+        {
+            return a + b;
+        }
+    }
+}
